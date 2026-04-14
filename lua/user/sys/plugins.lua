3c004989ab85229239a5863f9d8102f3d6b2299b
anoninus <anoninuxt@gmail.com>
2026-04-14 23:05:14 +0530

    fast

--- lua/user/sys/plugins.lua
+++ lua/user/sys/plugins.lua
@@ -436,6 +436,24 @@ require('lazy').setup({
     {
       'folke/tokyonight.nvim',
       commit = '5da1b76',
+      lazy = false, -- important for colorschemes
+      priority = 1000, -- ensure it loads first
+
+      opts = {
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          functions = { italic = false },
+          variables = { italic = false },
+        },
+        sidebars = 'dark',
+        floats = 'dark',
+      },
+
+      config = function(_, opts)
+        require('tokyonight').setup(opts)
+        vim.cmd.colorscheme('tokyonight-night')
+      end,
     },
   },
   -- ============================
