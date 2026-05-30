d3a7bd26f84644da3f8533dfa806056dee790b3d
anoninus <anoninuxt@gmail.com>
2026-05-30 20:01:46 +0530

    Colorized

--- lua/user/ui/core/statusline.lua
+++ lua/user/ui/core/statusline.lua
@@ -2,23 +2,21 @@ _G.lsp_status = function()
     local clients = vim.lsp.get_clients({ bufnr = 0 })
     if #clients == 0 then return "" end
 
-    -- LSP names
     local names = {}
     for _, c in ipairs(clients) do table.insert(names, c.name) end
 
-    -- Diagnostics
     local e = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
     local w = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
     local h = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
     local i = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
 
     local diag = ""
-    if e > 0 then diag = diag .. " [E:" .. e .. "]" end
-    if w > 0 then diag = diag .. " [W:]" .. w .. "]" end
-    if h > 0 then diag = diag .. " [H:]" .. h .. "]" end
-    if i > 0 then diag = diag .. " [I:]" .. i .. "]" end
+    if e > 0 then diag = diag .. "%#DiagnosticError# [E:" .. e .. "]%*" end
+    if w > 0 then diag = diag .. "%#DiagnosticWarn# [W:" .. w .. "]%*" end
+    if h > 0 then diag = diag .. "%#DiagnosticHint# [H:" .. h .. "]%*" end
+    if i > 0 then diag = diag .. "%#DiagnosticInfo# [I:" .. i .. "]%*" end
 
     return " [" .. table.concat(names, ",") .. "]" .. diag
 end
 
-vim.opt.statusline = "%f %h%m%r%=%{v:lua.lsp_status()}  %-14.(%l,%c%V%) %P"
+vim.opt.statusline = "%f %h%m%r%=%{%v:lua.lsp_status()%}  %-14.(%l,%c%V%) %P"
