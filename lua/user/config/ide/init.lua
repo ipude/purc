55db992daf679a3ea0a8475667510075fee53c1c
anoninus <anoninuxt@gmail.com>
2026-06-01 08:05:18 +0530

    Better mini clue setup

--- lua/user/config/ide/init.lua
+++ lua/user/config/ide/init.lua
@@ -2,7 +2,7 @@
 vim.schedule(function()
     require('user.config.ide.ide.module_require.autosave')
     require('user.config.ide.ide.module_require.run')
-    require('user.config.ide.ide.whkey')
+    require('user.config.ide.ide.miniclues')
     require('user.config.ide.ide.undotree')
     require('user.config.ide.file.fzf')
     require('user.config.ide.file.oil')
