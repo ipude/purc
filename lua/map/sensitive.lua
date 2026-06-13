b7dc212d24a95023407b69fe508e817cb5228a09
anoninus <anoninuxt@gmail.com>
2026-06-13 20:44:58 +0530

    same

---
+++ lua/map/sensitive.lua
@@ -0,0 +1,12 @@
+-- This map is ment to intercept <UP> and <DOWN> keys in Normal and Insert mode.
+--
+
+vim.keymap.set({'n', 'i'}, '<Up>', '<Nop>')
+vim.keymap.set({'n', 'i'}, '<Down>', '<Nop>')
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "o", "r" })
+  end,
+})
