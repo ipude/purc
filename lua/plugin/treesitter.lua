5d6fe7d5ca43c741799d5ab5b98058a330f96ab0
anoninus <anoninuxt@gmail.com>
2026-06-18 09:32:25 +0530

    Parsers load after VinEnter (once) then switches to Filetype.

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -3,57 +3,28 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = true,
     build = ":TSUpdate",
-    event = "BufEnter",
+    cmd = "TSInstall",
 
     config = function()
-      vim.defer_fn(function()
-        -- 1. Load & install
-        require("nvim-treesitter").setup({
-          install_dir = vim.fn.stdpath("data") .. "/site",
+      require("nvim-treesitter").setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+
+      require("nvim-treesitter")
+        .install({
+          "lua",
+          "javascript",
+          "zig",
+          "go",
+          "python",
+          "html",
+          "css",
+          "rust",
+          "json",
+          "markdown",
+          "toml",
         })
-
-        require("nvim-treesitter")
-          .install({
-            "lua",
-            "javascript",
-            "zig",
-            "go",
-            "python",
-            "html",
-            "css",
-            "rust",
-            "json",
-            "markdown",
-            "toml",
-          })
-          :wait(300000)
-
-        -- 2. Start treesitter on current buffer
-        pcall(vim.treesitter.start)
-
-        -- 3. Re-apply config to current window/buffer directly
-        local wo = vim.wo
-        local bo = vim.bo
-        wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        wo.foldmethod = "expr"
-        wo.foldlevel = 99
-        bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-
-        -- 4. Future buffers via FileType
-        vim.api.nvim_create_autocmd("FileType", {
-          callback = function()
-            local ok = pcall(vim.treesitter.start)
-            if not ok then
-              return
-            end
-
-            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-            vim.wo.foldmethod = "expr"
-            vim.wo.foldlevel = 99
-            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-          end,
-        })
-      end, 0)
+        :wait(300000)
     end,
   },
 }
