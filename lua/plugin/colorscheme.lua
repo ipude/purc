c6a0e295d5208c405a891811132543071cca6f1e
anoninus <anoninuxt@gmail.com>
2026-06-21 14:45:15 +0530

    Set statusline to color of Normal bg and made MsgSeparator bg good

--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -20,9 +20,11 @@ return {
         dim_inactive = false,
         on_highlights = function(hl, c)
           hl.Comment = { fg = "#809ab0", italic = false }
-          hl.LineNr = { fg = "#6b7a8e"}
-          hl.LineNrAbove = { fg = "#6b7a8e"}
-          hl.LineNrBelow = { fg = "#6b7a8e"}
+          hl.LineNr = { fg = "#6b7a8e" }
+          hl.LineNrAbove = { fg = "#6b7a8e" }
+          hl.LineNrBelow = { fg = "#6b7a8e" }
+          hl.MsgSeparator = { bg = "#809ab0" }
+          hl.Statusline = { bg = "#222436" }
         end,
       })
       vim.cmd.colorscheme("tokyonight-moon")
