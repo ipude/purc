01f1a53dd1dc1dee6a1c91b7c4c209fdfc3b9c39
pudep <261843105+pudep@users.noreply.github.com>
2026-07-09 14:10:17 +0530

    Mapped the S(visual) mode to <C-s> (viusal) for nvim-surround to eliminate the bug where accidentaly pressing S in expanded vim snippets cause lsp especially rust analyzer to panic and exit.

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -4,9 +4,11 @@
 return {
   {
     "kylechui/nvim-surround",
-    keys = { "ys", "ds", "cs", { "S", mode = "v" } },
+    keys = { "ys", "ds", "cs", { "<C-s>", mode = "v" } },
     config = function()
       require("nvim-surround").setup({})
+      vim.keymap.del("v", "S")
+      vim.keymap.set("v", "<C-s>", "<Plug>(nvim-surround-visual)", { desc = "Add surround" })
     end,
   },
 }
