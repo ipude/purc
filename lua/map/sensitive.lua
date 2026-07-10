5933d78adc1e908b46522f12302cb4fbeab553ee
pudep <261843105+pudep@users.noreply.github.com>
2026-07-10 09:54:22 +0530

    commented sensitive.lua

--- lua/map/sensitive.lua
+++ lua/map/sensitive.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "o", "r" })
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "*",
+--   callback = function()
+--     vim.opt_local.formatoptions:remove({ "o", "r" })
+--   end,
+-- })
