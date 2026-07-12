838981fc53e5e809bf9d1d869bac8f9c4cedd0c0
pudep <261843105+pudep@users.noreply.github.com>
2026-07-12 14:39:11 +0530

    lazygit scaling == full size of screen

--- lua/plugin/explorer.lua
+++ lua/plugin/explorer.lua
@@ -132,7 +132,7 @@ return {
     },
     config = function()
       vim.g.lazygit_floating_window_winblend = 0
-      vim.g.lazygit_floating_window_scaling_factor = 0.9
+      vim.g.lazygit_floating_window_scaling_factor = 1
       vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
       vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote not installed
     end,
